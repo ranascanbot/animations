4aa6227b0fa0f7103df4ffbf12bca559c12c6b2f
ranascanbot <rana@scanbot.io>
2024-01-22 13:14:22 +0100

    completed chapter-16

---
+++ 16-gradient-animations/SlideToReveal/AnimatedMaskLabel.swift
@@ -0,0 +1,106 @@
+/// Copyright (c) 2022-present Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import UIKit
+import QuartzCore
+
+@IBDesignable
+class AnimatedMaskLabel: UIView {
+  
+  let textAttributes: [NSAttributedString.Key: Any] = {
+    let style = NSMutableParagraphStyle()
+    style.alignment = .center
+    return [.font: UIFont.systemFont(ofSize: 28, weight: .thin), .paragraphStyle: style]
+  }()
+  
+  let gradientLayer: CAGradientLayer = {
+    
+    let gradientLayer = CAGradientLayer()
+    
+    // Configure the gradient here
+    gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+    gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+    
+    let colors = [
+      UIColor.black.cgColor,
+      UIColor.white.cgColor,
+      UIColor.black.cgColor
+    ]
+    gradientLayer.colors = colors
+    
+    let locations: [NSNumber] = [
+      0.25,
+      0.5,
+      0.75
+    ]
+    gradientLayer.locations = locations
+    
+    return gradientLayer
+  }()
+
+  @IBInspectable var text: String = "" {
+    didSet {
+      setNeedsDisplay()
+      let image = UIGraphicsImageRenderer(size: bounds.size).image { _ in
+          text.draw(in: bounds, withAttributes: textAttributes)
+      }
+      let maskLayer = CALayer()
+      maskLayer.backgroundColor = UIColor.clear.cgColor
+      maskLayer.frame = bounds.offsetBy(dx: bounds.size.width, dy: 0)
+      maskLayer.contents = image.cgImage
+      
+      gradientLayer.mask = maskLayer
+    }
+  }
+
+  override func layoutSubviews() {
+    layer.borderColor = UIColor.green.cgColor
+    gradientLayer.frame = bounds
+    gradientLayer.frame = CGRect(x: -bounds.size.width,
+                                 y: bounds.origin.y,
+                                 width: 3 * bounds.size.width,
+                                 height: bounds.size.height)
+  }
+
+  override func didMoveToWindow() {
+    super.didMoveToWindow()
+    layer.addSublayer(gradientLayer)
+    
+    let gradientAnimation = CABasicAnimation(keyPath: "locations")
+    gradientAnimation.fromValue = [0.0, 0.0, 0.25]
+    gradientAnimation.toValue = [0.75, 1.0, 1.0]
+    gradientAnimation.duration = 3.0
+    gradientAnimation.repeatCount = Float.infinity
+    
+    gradientLayer.add(gradientAnimation, forKey: nil)
+  }
+}
